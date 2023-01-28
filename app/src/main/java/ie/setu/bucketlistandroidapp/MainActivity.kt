7db7546aad8fb8cd6aa22f1ef74613a903808625
ChristianZaccaria <christian.zaccaria.cz@gmail.com>
2023-01-28 16:27:25 +0000

    - Changed colours of app - Added button that will be used to sign-in as a guest - Added an image to main activity - Updated .gitignore - Added viewBinding build features to Gradle

--- app/src/main/java/ie/setu/bucketlistandroidapp/MainActivity.kt
+++ app/src/main/java/ie/setu/bucketlistandroidapp/MainActivity.kt
@@ -2,10 +2,23 @@ package ie.setu.bucketlistandroidapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import ie.setu.bucketlistandroidapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.guestButton.setOnClickListener {
+            val guestButtonText = getString(R.string.guest_button_text)
+            Toast.makeText(applicationContext, guestButtonText, Toast.LENGTH_LONG).show()
+        }
     }
+
 }
