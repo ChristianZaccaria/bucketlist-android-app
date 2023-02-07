1c976e39834774b9e303e6321678e63456f9daf4
ChristianZaccaria <christian.zaccaria.cz@gmail.com>
2023-02-07 00:11:14 +0000

    Add Calendar Picker for dueDate field

--- app/src/main/java/ie/setu/bucketlistandroidapp/models/ExperienceModel.kt
+++ app/src/main/java/ie/setu/bucketlistandroidapp/models/ExperienceModel.kt
@@ -1,5 +1,7 @@
 package ie.setu.bucketlistandroidapp.models
 
+import java.util.*
+
 // TODO: image will use URI soon
 data class ExperienceModel(
     var title: String = "",
@@ -8,5 +10,5 @@ data class ExperienceModel(
     var location: String = "",
     var image: String = "",
     var cost: Double = 0.00,
-//    var dueDate: Date = Date(0), TODO: more investigation needed on calendar picker
+    var dueDate: Date = Date(0),
     var achieved: Boolean = false)
